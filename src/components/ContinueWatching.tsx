61374c361fb0a8f0821b978e32638f9e5d14ff44
esaia <esaiagafrindashvili@gmail.com>
2024-12-07 18:21:00 +0400

    fix: errors in continue watching component

--- src/components/ContinueWatching.tsx
+++ src/components/ContinueWatching.tsx
@@ -6,13 +6,13 @@ import 'swiper/css';
 import 'swiper/css/autoplay';
 import Image from 'next/image';
 import { MdTheaters } from 'react-icons/md';
-import { continueWatchingType } from '@/types';
+import { continueWatchingType, MovieType } from '@/types';
 import Link from 'next/link';
 import { generateSlug } from '@/lib/helpers';
 
 const ContinueWatching = () => {
   const [isClient, setIsClient] = useState(false);
-  const [movies, setMovies] = useState<continueWatchingType[]>();
+  const [movies, setMovies] = useState<MovieType[]>();
 
   const calculatePercentage = (part: number, total: number) => {
     if (total === 0) return 0;
@@ -21,22 +21,22 @@ const ContinueWatching = () => {
     return `${procentage || 0}%`;
   };
 
-  useEffect(() => {
-    setIsClient(true);
-    const continue_watching_movies = localStorage.getItem('continue_watching_movies');
-    const continue_watching_series = localStorage.getItem('continue_watching_series');
+  // useEffect(() => {
+  //   setIsClient(true);
+  //   const continue_watching_movies = localStorage.getItem('continue_watching_movies');
+  //   const continue_watching_series = localStorage.getItem('continue_watching_series');
 
-    if (continue_watching_movies || continue_watching_series) {
-      setMovies(
-        (
-          [
-            ...JSON.parse(continue_watching_movies || '[]'),
-            ...JSON.parse(continue_watching_series || '[]'),
-          ] as continueWatchingType[]
-        ).sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()),
-      );
-    }
-  }, []);
+  //   if (continue_watching_movies || continue_watching_series) {
+  //     setMovies(
+  //       (
+  //         [
+  //           ...JSON.parse(continue_watching_movies || '[]'),
+  //           ...JSON.parse(continue_watching_series || '[]'),
+  //         ] as continueWatchingType[]
+  //       ).sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()),
+  //     );
+  //   }
+  // }, []);
 
   return (
     <>
@@ -55,13 +55,12 @@ const ContinueWatching = () => {
                 return (
                   <SwiperSlide className="relative !w-[260px] cursor-pointer pr-2 lg:!w-[440px] lg:pr-6" key={movie.id}>
                     <Link
-                      href={`/${movie?.season ? 'serie' : 'movie'}/${generateSlug(movie)}`}
-                      className="group relative block !h-[140px] overflow-hidden rounded-sm lg:!h-[280px]"
-                    >
+                      href={`/movie/${generateSlug(movie)}`}
+                      className="group relative block !h-[140px] overflow-hidden rounded-sm lg:!h-[280px]">
                       <Image
                         className="absolute left-0 top-0 h-full w-full overflow-hidden object-cover transition-all duration-500 group-hover:scale-125 group-hover:opacity-50 group-hover:blur-sm"
-                        src={`${process.env.NEXT_PUBLIC_URL}/storage/${movie?.cover_image}`}
-                        alt={movie.title_ka}
+                        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                        alt={movie.title}
                         width={200}
                         height={400}
                       />
@@ -69,15 +68,14 @@ const ContinueWatching = () => {
                       <div className="absolute bottom-0 left-0 h-full w-full scale-105 bg-gradient-to-b from-black/40 via-gray-900/20 to-black/50" />
 
                       <div className="absolute bottom-0 left-0 flex h-20 w-full flex-col items-start justify-center bg-black/50 px-2 py-3 md:h-24 lg:h-28">
-                        <p className="text line-clamp-2">{movie.title_ka}</p>
-                        <p className="text line-clamp-2">{movie.title_en}</p>
+                        <p className="text line-clamp-2">{movie.title}</p>
                       </div>
                     </Link>
 
-                    <div
+                    {/* <div
                       className="absolute bottom-0 left-0 h-1 bg-secondary"
                       style={{ width: calculatePercentage(movie?.time, movie?.duration) }}
-                    ></div>
+                    ></div> */}
                   </SwiperSlide>
                 );
               })}
